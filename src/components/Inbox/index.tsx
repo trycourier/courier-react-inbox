623b9fdc22c5cb4b12a9f9a761d55d8c797fe2c0
RoyalSix <jay.a.scottjr@biola.edu>
2021-01-19 16:22:54 -0800

    init

---
+++ src/components/Inbox/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container} from './styled';
+
+function Inbox() {
+  return (
+    <Container>
+      inbox
+    </Container>
+  )
+}
+
+export default Inbox
---
+++ stories/Inbox.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Inbox } from "../src";
+
+export default {
+  title: "Example/Inbox",
+  component: Inbox,
+  argTypes:{ },
+  args: {},
+};
+
+export function Default() {
+  return <Inbox />
+}
