7d913dfd82c9830e53fe46fe05c19284bbef1ef8
RoyalSix <jay.a.scottjr@biola.edu>
2021-01-19 16:29:12 -0800

    added build

---
+++ dist/components/Inbox/styled.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Container = void 0;
+
+var _styledComponents = _interopRequireDefault(require("styled-components"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var Container = _styledComponents["default"].div.withConfig({
+  displayName: "styled__Container",
+  componentId: "sc-14oug27-0"
+})([""]);
+
+exports.Container = Container;
